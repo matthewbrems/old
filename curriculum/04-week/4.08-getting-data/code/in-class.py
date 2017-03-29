b172f794e05785cd726e21f197c2706572124535
Joseph Nelson <joseph.nelson2012@gmail.com>
2017-03-29 13:57:16 -0400

    updated in-class

--- curriculum/04-week/4.08-getting-data/code/in-class.py
+++ curriculum/04-week/4.08-getting-data/code/in-class.py
@@ -1,16 +1,3 @@
 # make a request
-import requests
-r = requests.get('http://www.abrahamlincolnonline.org/lincoln/speeches/quotes.htm')
+http://www.abrahamlincolnonline.org/lincoln/speeches/quotes.htm
 
-# verify the success
-r.status_code
-
-# parse the HTML
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(r.text)
-
-# print it out
-quotes = soup.find_all(name='p')
-
-for quote in quotes:
-    print quote.find(name='b')
